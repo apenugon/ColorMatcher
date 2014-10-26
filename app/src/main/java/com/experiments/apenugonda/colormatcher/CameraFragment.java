4d9409db2e6b4a4cb48b8c142603fde9828f2c5d
ragnarok451 <akulvp@gmail.com>
2014-10-26 00:50:33 -0400

    Made references smoother, got rid of potential memory leak

---
+++ app/src/main/java/com/experiments/apenugonda/colormatcher/CameraFragment.java
@@ -0,0 +1,80 @@
+package com.experiments.apenugonda.colormatcher;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+/**
+ * A fragment that allows for getting Camera input
+ */
+public class CameraFragment extends Fragment {
+    private CameraView mPreview;
+    private Camera mCamera;
+    private Activity thisActivity;
+
+    public Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e) {
+            // Alert that camera is not available
+        }
+        return c;
+    }
+
+    public CameraFragment() {
+        mCamera = getCameraInstance();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sample_camera_view, container, false);
+
+        return view;
+        //View rootView = inflater.inflate(R.layout.fragment_select_color, container, false);
+        //return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        try {
+            mCamera.reconnect();
+        } catch (Exception e) {
+            mCamera = getCameraInstance();
+            Log.d("Camera", "Failed to reconnect to Camera: " + e.toString());
+        }
+        //Set orientation to always be vertical
+        mCamera.setDisplayOrientation(90);
+
+        mPreview = new CameraView(thisActivity, mCamera);
+        FrameLayout layout = (FrameLayout)thisActivity.findViewById(R.id.camera_preview);
+        layout.addView(mPreview);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mCamera.stopPreview();
+        FrameLayout layout = (FrameLayout)thisActivity.findViewById(R.id.camera_preview);
+        layout.removeView(mPreview);
+        mCamera.release();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        thisActivity = activity;
+        ((SelectColor) activity).onSectionAttached(1); //First Activity
+        // TODO: Change call to use a stored number
+    }
+}
--- app/src/main/java/com/experiments/apenugonda/colormatcher/SelectColor.java
+++ app/src/main/java/com/experiments/apenugonda/colormatcher/SelectColor.java
@@ -4,16 +4,13 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 
 public class SelectColor extends Activity
@@ -171,75 +168,6 @@ public class SelectColor extends Activity
             super.onAttach(activity);
             ((SelectColor) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
-    /**
-     * A fragment that allows for getting Camera input
-     */
-    public class CameraFragment extends Fragment {
-        private CameraView mPreview;
-        private Camera mCamera;
-        private Activity thisActivity;
-
-        public Camera getCameraInstance() {
-            Camera c = null;
-            try {
-                c = Camera.open();
-            }
-            catch (Exception e) {
-                // Alert that camera is not available
-            }
-            return c;
-        }
-
-        public CameraFragment() {
-            mCamera = getCameraInstance();
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.sample_camera_view, container, false);
-
-            return view;
-            //View rootView = inflater.inflate(R.layout.fragment_select_color, container, false);
-            //return rootView;
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-
-            try {
-                mCamera.reconnect();
-            } catch (Exception e) {
-                mCamera = getCameraInstance();
-                Log.d("Camera", "Failed to reconnect to Camera: " + e.toString());
-            }
-            //Set orientation to always be vertical
-            mCamera.setDisplayOrientation(90);
-
-            mPreview = new CameraView(thisActivity, mCamera);
-            FrameLayout layout = (FrameLayout)findViewById(R.id.camera_preview);
-            layout.addView(mPreview);
-        }
-
-        @Override
-        public void onPause() {
-            super.onPause();
-
-            mCamera.stopPreview();
-            FrameLayout layout = (FrameLayout)findViewById(R.id.camera_preview);
-            layout.removeView(mPreview);
-            mCamera.release();
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            thisActivity = activity;
-            ((SelectColor) activity).onSectionAttached(1); //First Activity
-            // TODO: Change call to use a stored number
         }
     }
 
